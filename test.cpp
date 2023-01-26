7bdf54e52446f7e4232e4a85ef769159ac47a4b6
owalsh <owalsh@student.42.fr>
2023-01-26 15:50:59 +0100

    Add reverse_iterator

--- test.cpp
+++ test.cpp
@@ -6,7 +6,7 @@
 /*   By: owalsh <owalsh@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/24 21:58:20 by owalsh            #+#    #+#             */
-/*   Updated: 2023/01/25 14:38:48 by owalsh           ###   ########.fr       */
+/*   Updated: 2023/01/26 15:45:53 by owalsh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,40 +18,43 @@
 	#include <vector>
 	namespace ft = std;
 #else
+	#include "iterator.hpp"
 	#include <vector.hpp>
 #endif
 
-template <typename T>
-typename ft::enable_if<ft::is_integral<T>::value, T>::type add(T a, T b)
-{
-    return a + b;
-}
+// template <typename T>
+// typename ft::enable_if<ft::is_integral<T>::value, T>::type add(T a, T b)
+// {
+//     return a + b;
+// }
 
-template <typename T>
-typename ft::enable_if<!ft::is_integral<T>::value, T>::type add(T a, T b)
-{
-	(void)a;
-	(void)b;
-    std::cout << "This function is only available for integral types." << std::endl;
-    return T();
-}
+// template <typename T>
+// typename ft::enable_if<!ft::is_integral<T>::value, T>::type add(T a, T b)
+// {
+// 	(void)a;
+// 	(void)b;
+//     std::cout << "This function is only available for integral types." << std::endl;
+//     return T();
+// }
 
 int main()
 {
-	std::cout << "***TESTING IS_INTEGRAL TEMPLATE***" << "\n" << "\n";
-	std::cout << std::boolalpha;
-	std::cout << "char : " << ft::is_integral<char>::value << std::endl;
-	std::cout << "const char : " << ft::is_integral<const char >::value << std::endl;
-	std::cout << "const volatile int : " << ft::is_integral<const volatile int>::value << std::endl;
-	std::cout << "float : " << ft::is_integral<float>::value << "\n";
-	std::cout << "int : " << ft::is_integral<int>::value << "\n";
-	std::cout << "ft::vector<int> : " << ft::is_integral<ft::vector<int> >::value << "\n";
-	std::cout << "short : " << ft::is_integral<short>::value << "\n";
-	std::cout << "***" << "\n";
+	// std::cout << "***TESTING IS_INTEGRAL TEMPLATE***" << "\n" << "\n";
+	// std::cout << std::boolalpha;
+	// std::cout << "char : " << ft::is_integral<char>::value << std::endl;
+	// std::cout << "const char : " << ft::is_integral<const char >::value << std::endl;
+	// std::cout << "const volatile int : " << ft::is_integral<const volatile int>::value << std::endl;
+	// std::cout << "float : " << ft::is_integral<float>::value << "\n";
+	// std::cout << "int : " << ft::is_integral<int>::value << "\n";
+	// std::cout << "ft::vector<int> : " << ft::is_integral<ft::vector<int> >::value << "\n";
+	// std::cout << "short : " << ft::is_integral<short>::value << "\n";
+	// std::cout << "***" << "\n";
 
-	std::cout << "***TESTING ENABLE IF***" << "\n\n";
-	std::cout << add(4, 2) << std::endl;
-    std::cout << add("hoho", "hihi") << std::endl;
+	// std::cout << "***TESTING ENABLE IF***" << "\n\n";
+	// std::cout << add(4, 2) << std::endl;
+    // std::cout << add("hoho", "hihi") << std::endl;
 	
+
+								  
 	return 0;
 }
--- utils/iterator.hpp
+++ utils/iterator.hpp
@@ -6,7 +6,7 @@
 /*   By: owalsh <owalsh@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/25 15:18:10 by owalsh            #+#    #+#             */
-/*   Updated: 2023/01/25 17:49:19 by owalsh           ###   ########.fr       */
+/*   Updated: 2023/01/26 15:44:35 by owalsh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,8 +46,166 @@ namespace ft
 		typedef const	Tp&								reference;
 	};
 	
-	
+	/* ------------- reverse iterator ------------- */
+	template <class Iter>
+	class reverse_iterator
+	{
 
+		protected:
+			Iter 														current;
+			typedef				ft::iterator_traits<Iter>				__traits_type;
+		
+		public:
+			typedef				Iter									iterator_type;
+			typedef typename	__traits_type::iterator_category		iterator_category;
+			typedef typename	__traits_type::value_type				value_type;
+			typedef typename	__traits_type::difference_type			difference_type;
+			typedef typename	__traits_type::pointer					pointer;
+			typedef	typename	__traits_type::reference				reference;
+
+			reverse_iterator() : current() { }
+			explicit
+			reverse_iterator(iterator_type __x) : current(__x) { }
+			reverse_iterator(const reverse_iterator& __x) : current(__x.current) { }
+			template <class _Iter>
+				reverse_iterator(const reverse_iterator<_Iter>& __x)
+				: current(__x.base()) { }
+
+			template <class _Iter>
+			reverse_iterator& operator=(const reverse_iterator<_Iter>& copy)
+			{
+				*current = copy.base();
+			}
+			
+			iterator_type base() const
+			{
+				return current;
+			}
+			
+			reference operator*() const
+			{
+				Iter __tmp = current;
+				return *--__tmp;
+			}
+
+			pointer operator->() const
+			{
+				return &(operator*());
+			}
+
+			reverse_iterator& operator++()
+			{
+				--current;
+				return *this;	
+			}
+
+			reverse_iterator operator++(int)
+			{
+				reverse_iterator __tmp = *this;
+				--current;
+				return __tmp;
+			}
+
+			reverse_iterator& operator--()
+			{
+				++current;
+				return *this;
+			}
+
+			reverse_iterator operator--(int)
+			{
+				reverse_iterator __tmp = *this;
+				++current;
+				return __tmp;
+			}
+
+			reverse_iterator operator+(difference_type __n) const
+			{
+				return reverse_iterator(current - __n);
+			}
+
+			reverse_iterator& operator+=(difference_type __n)
+			{
+				current -= __n;
+				return *this;
+			}
+
+			reverse_iterator operator-(difference_type __n) const
+			{
+				return reverse_iterator(current + __n);
+			}
+
+			reverse_iterator& operator-=(difference_type __n)
+			{
+				current += __n;
+				return *this;
+			}
+
+			reference operator[](difference_type __n) const
+			{
+				return *(*this + __n);
+			}
+			
+	};
+
+	template <typename _IteratorL, typename _IteratorR>
+		inline bool operator==(const reverse_iterator<_IteratorL>& lhs,
+								const reverse_iterator<_IteratorR>& rhs)
+		{
+			return lhs.base() == rhs.base();
+		}
+	
+	template <typename _IteratorL, typename _IteratorR>
+		bool operator!=(const reverse_iterator<_IteratorL>& lhs,
+								const reverse_iterator<_IteratorR>& rhs)
+		{
+			return !(lhs == rhs);
+		}
+	
+	template <typename _IteratorL, typename _IteratorR>
+		inline bool operator<(const reverse_iterator<_IteratorL>& lhs,
+								const reverse_iterator<_IteratorR>& rhs)
+		{
+			return rhs.base() < lhs.base();
+		}
+
+	template <typename _IteratorL, typename _IteratorR>
+		inline bool operator>(const reverse_iterator<_IteratorL>& lhs,
+								const reverse_iterator<_IteratorR>& rhs)
+		{
+			return rhs < lhs;
+		}
+	
+	template <typename _IteratorL, typename _IteratorR>
+		inline bool operator<=(const reverse_iterator<_IteratorL>& lhs,
+								const reverse_iterator<_IteratorR>& rhs)
+		{
+			return !(rhs < lhs);
+		}
+
+	template <typename _IteratorL, typename _IteratorR>
+		inline bool operator>=(const reverse_iterator<_IteratorL>& lhs,
+								const reverse_iterator<_IteratorR>& rhs)
+		{
+			return !(rhs > lhs);
+		}
+	
+	template <typename _IteratorL, typename _IteratorR>
+		inline typename reverse_iterator<_IteratorL>::difference_type
+		operator-(const reverse_iterator<_IteratorL>& lhs,
+					const reverse_iterator<_IteratorR>& rhs) 
+		{
+			return rhs.base() - lhs.base();
+		}
+
+	template <typename _IteratorL, typename _IteratorR>
+		inline typename reverse_iterator<_IteratorL>::difference_type
+		operator+(const reverse_iterator<_IteratorL>& lhs,
+					const reverse_iterator<_IteratorR>& rhs) 
+		{
+			return rhs.base() + lhs.base();
+		}
+		
 };
 
 #endif
